050f8aa52721e82c0f7cd7d16b75b5f6db2c6f15
Jim Wallace <jim@extroverteddeveloper.com>
2026-02-21 13:00:12 -0500

    feat: add FolderPickerView wrapping UIDocumentPickerViewController

---
+++ GifKeyboard/Views/FolderPickerView.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+import UniformTypeIdentifiers
+
+struct FolderPickerView: UIViewControllerRepresentable {
+    let onFolderSelected: (URL) -> Void
+
+    func makeUIViewController(context: Context) -> UIDocumentPickerViewController {
+        let picker = UIDocumentPickerViewController(forOpeningContentTypes: [.folder])
+        picker.delegate = context.coordinator
+        picker.allowsMultipleSelection = false
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator { Coordinator(onFolderSelected: onFolderSelected) }
+
+    class Coordinator: NSObject, UIDocumentPickerDelegate {
+        let onFolderSelected: (URL) -> Void
+        init(onFolderSelected: @escaping (URL) -> Void) { self.onFolderSelected = onFolderSelected }
+
+        func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+            guard let url = urls.first else { return }
+            onFolderSelected(url)
+        }
+    }
+}
